12d9dc2ceab3179755148be9d617bd37fdc2616b
mannaja <manman7852078520@gmail.com>
2024-01-02 16:01:28 +0700

    lab13_1 complete and change Readme

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,28 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat (const double A[] ,int N, double B[]){
+    double sum = 0, sumsqure = 0, multiGeometric = 1, sumHarmonic = 0, min = A[0], max = A[0];
+
+    for (int i = 0; i < N; i++){
+        sum += A[i];
+        sumsqure += pow(A[i], 2);
+        multiGeometric *= A[i];
+        sumHarmonic += 1.0/A[i];
+        max = max <= A[i] ? A[i] : max;
+        min = min >= A[i] ? A[i] : min;
+    }
+
+    double mean = sum / N;
+    double standardDeviation = sqrt((sumsqure / N) - pow(mean, 2));
+    double geometricMean = pow(multiGeometric, 1.0 / N); 
+    double HarmonicMean = N / sumHarmonic;
+
+    B[0] = mean;
+    B[1] = standardDeviation;
+    B[2] = geometricMean;
+    B[3] = HarmonicMean;
+    B[4] = max;
+    B[5] = min;
+}
